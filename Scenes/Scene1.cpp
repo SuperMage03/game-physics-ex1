b5d84955a911056bf1bcdc09f3cb9b3d904dfb11
Eston Li <liyuelin2004@gmail.com>
2024-11-16 22:24:14 +0100

    changed default values to the assignment requested values

--- Scenes/Scene1.cpp
+++ Scenes/Scene1.cpp
@@ -1,13 +1,13 @@
 #include "Scene1.h"
 #include <iostream>
 
+const float DEFAULT_POINT_MASS = 10.0f;
 const glm::vec3 INITIAL_P1_POSITION{ 0.0f,  0.0f,  0.0f};
 const glm::vec3 INITIAL_P1_VELOCITY{-1.0f,  0.0f,  0.0f};
 const glm::vec3 INITIAL_P2_POSITION{ 0.0f,  2.0f,  0.0f};
 const glm::vec3 INITIAL_P2_VELOCITY{ 1.0f,  0.0f,  0.0f};
 
-const float POINT_MASS = 1.0f;
-const float SPRING_STIFFNESS = 0.3f;
+const float SPRING_STIFFNESS = 40.0f;
 const float SPRING_INITIAL_LENGTH = 1.0f;
 
 const float STEP = 0.1f;
@@ -16,12 +16,14 @@ void Scene1::init() {
     PointRegistry* point_registry = PointRegistry::getInstance();
     ForceRegistry* force_registry = ForceRegistry::getInstance();
 
-    Point p1;
+    // Creating Points
+    Point p1{DEFAULT_POINT_MASS};
     point_registry->add(p1);
 
-    Point p2;
+    Point p2{DEFAULT_POINT_MASS};
     point_registry->add(p2);
 
+    // Creating Force Generators
     SpringForceGenerator sfg_p1{p1, SPRING_STIFFNESS, SPRING_INITIAL_LENGTH};
     force_registry->add(p2, sfg_p1);
 
@@ -37,12 +39,13 @@ void Scene1::init() {
     point_registry->setIntegrationMode(PointRegistry::IntegrationMode::EULER);
     point_registry->simulateStep(STEP);
 
-    std::cout << "EULER STEP RESULT:" << std::endl;
+    std::cout << "-------- EULER STEP RESULT --------" << std::endl;
     std::cout << "POINT 1:" << std::endl;
     std::cout << p1 << std::endl;
     std::cout << "POINT 2:" << std::endl;
     std::cout << p2 << std::endl;
 
+    // Set the initial position and velocity
     p1.setPosition(INITIAL_P1_POSITION);
     p1.setVelocity(INITIAL_P1_VELOCITY);
     p2.setPosition(INITIAL_P2_POSITION);
@@ -51,7 +54,7 @@ void Scene1::init() {
     point_registry->setIntegrationMode(PointRegistry::IntegrationMode::MIDPOINT);
     point_registry->simulateStep(STEP);
 
-    std::cout << "MIDPOINT STEP RESULT:" << std::endl;
+    std::cout << "------- MIDPOINT STEP RESULT -------" << std::endl;
     std::cout << "POINT 1:" << std::endl;
     std::cout << p1 << std::endl;
     std::cout << "POINT 2:" << std::endl;
