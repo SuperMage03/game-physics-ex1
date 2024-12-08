e9f24df104bdbacb3d552a9f8a48883e9217f410
SELB1 <94957228+SELB1@users.noreply.github.com>
2024-12-09 00:17:33 +0100

    Update Scene1_SingleStep.h

--- Scenes/Scene1_SingleStep.h
+++ Scenes/Scene1_SingleStep.h
@@ -13,16 +13,12 @@ public:
         glm::vec3 initialPosition = glm::vec3(0.0f);
         glm::quat initialOrientation = glm::angleAxis(glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         float mass = 2.0f;
-
-        // Define the dimensions of the cuboid
-        float width = 1.0f;  // Replace with actual width
-        float height = 0.6f; // Replace with actual height
+        float width = 1.0f; 
+        float height = 0.6f;
         float depth = 0.5f;  
 
-        // Create the RigidBody with the new constructor
         body = new RigidBody(initialPosition, initialOrientation, mass, width, height, depth);
 
-        // Apply force and calculate torque in the world frame
         glm::vec3 force = glm::vec3(1.0f, 1.0f, 0.0f);
         glm::vec3 applicationPoint = glm::vec3(0.3f, 0.5f, 0.25f);
         body->applyForce(force, applicationPoint);
